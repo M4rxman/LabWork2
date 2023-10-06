923ba94e2f127fcb96ba73dd7bd7070e4fa3abec
Olek <up202210478@fe.up.pt>
2023-10-06 12:31:10 +0100

    hot fix #1

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,26 +1,18 @@
-import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TabBehaviour;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import java.io.EOFException;
 import java.io.IOException;
 
 public class Game {
-    private int x = 10;
-    private int y = 10;
-    Hero _hero = new Hero(x,y);
     Screen _screen;
     Terminal _terminal;
     TerminalSize _terminalSize;
     DefaultTerminalFactory _terminalFactory;
+    Arena _arena;
 
     Game() {
         try {
@@ -32,54 +24,24 @@ public class Game {
             _screen.setCursorPosition(null); // we don't need a cursor
             _screen.startScreen(); // screens must be started
             _screen.doResizeIfNecessary(); // resize screen ifnecessary
-
+            _arena = new Arena(30, 16, _screen);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void processKey(KeyStroke keyStroke) throws IOException {
-        System.out.println(keyStroke);
-
-        switch (keyStroke.getKeyType()) {
-            case ArrowUp:
-                moveHero(_hero.moveUp());
-                break;
-            case ArrowDown:
-                moveHero(_hero.moveDown());
-                break;
-            case ArrowRight:
-                moveHero(_hero.moveRight());
-                break;
-            case ArrowLeft:
-                moveHero(_hero.moveLeft());
-                break;
-            case EOF:
-                throw new IOException("EOF exception");
-            default:
-                if (keyStroke.getCharacter() == 'q') {
-                    _screen.close();
-                }
-        }
+    private void processKey(KeyStroke key) throws IOException {
+        _arena.processKey(key);
     }
-
     private void draw(Screen screen) throws Exception {
-        try {
-
-            screen.clear();
-            _hero.heriocDrawing(screen);
-            screen.refresh();
-
-            KeyStroke key = screen.readInput();
-            processKey(key);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new Exception("End of the game");
-        }
+        screen.clear();
+        _arena.draw(screen);
+        screen.refresh();
     }
 
+    /*
+    */
     public void run() {
         while (true) {
             try {
@@ -89,10 +51,6 @@ public class Game {
                 break;
             }
         }
-    }
-
-    private void moveHero(Position position) {
-        _hero.setPosition(position);
     }
 
 }
